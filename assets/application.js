e62a95419c87bd213a1feaf48091a13bb4b36ba1
yourman <azzerino@VerasDad.com>
2021-08-07 21:56:04 +0200

    Adding the new recomendations section

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,16 @@
-// Put your application javascript here
+// Loads the recomendations asynchronously (another option to add the recomendations)
+// sections.register('hero', theme.HeroSection);
+// sections.register('product-recommendations', theme.ProductRecommendations);
+// theme.ProductRecommendations = (function() {
+//   function ProductRecommendations(container) {
+//     var $container = (this.$container = $(container));
+//     var baseUrl = $container.data('baseUrl');
+//     var productId = $container.data('productId');
+//     var limit = $container.data('limit');
+//     var productRecommendationsUrlAndContainerClass = baseUrl + '?section_id=product-recommendations&limit=' + limit +
+//       '&product_id=' +productId +
+//       ' .product-recommendations';
+//     $container.parent().load(productRecommendationsUrlAndContainerClass);
+//   }
+//   return ProductRecommendations;
+// })();
