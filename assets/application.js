89542869625639d605777a33989592e7e1f2ebf5
Anna Leyn <annaleyn.twitch@gmail.com>
2021-08-20 20:09:58 +0300

    added "select" to collection page / some changes at hero

--- assets/application.js
+++ assets/application.js
@@ -14,3 +14,12 @@
 //   }
 //   return ProductRecommendations;
 // })();
+document.addEventListener('DOMContentLoaded', () => { 
+    console.log('The page is loaded')
+        document.querySelector('#sort_by').addEventListener('change', function(e) {
+        var url = new URL(window.location.href);
+        url.searchParams.set('sort_by', e.currentTarget.value);
+
+        window.location = url.href;
+    })
+  })
